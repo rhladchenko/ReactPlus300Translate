72d163ec8c32051603b082b6142727efa8d20688
rhladchenko <ruslan.hladchenko@cpcs.ws>
2021-01-08 13:04:58 +0200

    Start translate

---
+++ translate/src/components/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+
+class App extends React.Component {
+    state = {
+        language: 'english',
+    };
+
+    onLanguageChange = (language) => {
+        {
+            this.setState({ language: language });
+        }
+    };
+
+    render() {
+        return (
+            <div className='ui container'>
+                <div>
+                    Select language:
+                    <i
+                        className='flag us'
+                        onClick={() => {
+                            this.onLanguageChange('english');
+                        }}
+                    />
+                    <i
+                        className='flag ru'
+                        onClick={() => {
+                            this.onLanguageChange('russian');
+                        }}
+                    />
+                </div>
+                <LanguageContext.Provider value={this.state.language}>
+                    <UserCreate />
+                </LanguageContext.Provider>
+            </div>
+        );
+    }
+}
+
+export default App;
